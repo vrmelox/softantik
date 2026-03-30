4830cbc2dc5a575e5a6e887f4739d5cbc8702a56
vrmelo <akandeabiodoun@gmail.com>
2026-03-30 17:49:57 +0100

    feat: add full_adder logic to combinatorial gates

--- src/gates/combinatorial.rs
+++ src/gates/combinatorial.rs
@@ -11,3 +11,7 @@ pub fn dmux(ain: Bit, s:Bit) -> (Bit, Bit) {
 pub fn half_adder(a: Bit, b: Bit) -> (Bit, Bit) {
     (xor(a, b), and(a, b))
 }
+
+pub fn full_adder(a: Bit, b: Bit, cin: Bit) -> (Bit, Bit) {
+    (xor(xor(a, b), cin), or(and(a, b), and(xor(a, b), cin)))
+}
