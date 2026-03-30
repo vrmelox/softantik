17efe461a461e73514025cba025fb4dccd6e396c
vrmelo <akandeabiodoun@gmail.com>
2026-03-30 18:36:13 +0100

    feat: implement 16-bit addition logic in ALU module

--- src/gates/alu.rs
+++ src/gates/alu.rs
@@ -1,3 +1,9 @@
-use super::
+use super::*
 
-pub fn alu16(a: [])
+pub fn add16(a: [Bit;16], b:[Bit;16]) -> [Bit;16] {
+    let mut results = Vec::new();
+    results.push(half_adder(a[0], b[0]))
+    for i in 1..14 {
+        results.push()
+    }
+}
